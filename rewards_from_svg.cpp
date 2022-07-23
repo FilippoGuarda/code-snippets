5603b86576f46e079a216e623559321e42014543
FilippoGuarda <filippo.guarda96@gmai.com>
2022-07-23 18:14:14 +0200

    new file created

---
+++ rewards_from_svg.cpp
@@ -0,0 +1,10 @@
+
+/* in launch file
+TODO: read svg file and send values to a matrix 
+create an irradiation matrix the same size of the svg file */
+
+
+/* in node file
+1) ask for robot position
+2) compute irradiation of environment and update irradiation matrix accordingly
+3) compute reward based on irradiation */
